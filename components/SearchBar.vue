fdb6d27cf5f5f9153bd355ee39f8a7d6e0c2aa7b
Henrik Askjer <Henrik.Askjer@uib.no>
2022-08-02 12:45:53 +0200

    combobox

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,6 +1,23 @@
 <template>
   <v-form role="search" class="autocomplete-container" :class="$vuetify.display.name">
-    <v-autocomplete>
-    </v-autocomplete>
+    <v-combobox 
+        autofocus
+        solo
+        clearable
+        compact
+        full-width
+        :prepend-inner-icon="mdiMagnify"
+        color="primary">
+    </v-combobox>
   </v-form>
 </template>
+
+<script>
+  import { mdiMagnify   } from '@mdi/js'
+
+  export default {
+    data: () => ({
+      mdiMagnify 
+    })
+  }
+</script>

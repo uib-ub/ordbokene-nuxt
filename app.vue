0b1b2e9cdf0e8ba60e858fda93b615d1c4932696
Henrik Askjer <Henrik.Askjer@uib.no>
2022-08-08 13:47:41 +0200

    sass customization

--- app.vue
+++ app.vue
@@ -35,16 +35,24 @@
     </div>
   </div>
 </nav>
+<br>
 <button  type="button" class="btn btn-secondary pill">Test</button>
+
+
+<div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul></div>
+
     <NuxtPage />
 </template>
 
 <script setup lang="ts">
-const { $bootstrap } = useNuxtApp();
-let modal: Modal;
-onMounted(() => {
-  modal = new $bootstrap.Modal(document.getElementById("navbar-main"));
-});
 
 </script>
 

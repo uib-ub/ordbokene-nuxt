6855f9ee10ad12f8103054dff75f9696c9c0348f
Silje Scholl <silje.scholl@uib.no>
2022-08-22 14:46:20 +0200

    test- set to localstorage

--- stores/settingsStore.ts
+++ stores/settingsStore.ts
@@ -1,9 +1,14 @@
 import { defineStore } from "pinia";
 
-export const useSettingsStore = defineStore('settings', {
-    state: () => ({
-        inflectionExpanded: false,
-        inflectionNo: false,
-        inflectionTableContext: false
-    })
+export const useSettingsStore = defineStore('settings', () => {
+    const inflectionExpanded = ref(false);
+    const inflectionNo = ref(false);
+    const inflectionTableContext = ref(false);
+
+        onMounted(() => {
+            watch(inflectionExpanded, (state) => {
+                console.log('hei')
+                localStorage.setItem('settings-state', JSON.stringify(state))
+              }) 
+        })
 })

2fee8936a8d8dfaf7c49025884bea245247d6a0d
Silje Scholl <silje.scholl@uib.no>
2022-08-23 14:24:44 +0200

    feat: save settings to localstorage

--- stores/settingsStore.ts
+++ stores/settingsStore.ts
@@ -5,10 +5,29 @@ export const useSettingsStore = defineStore('settings', () => {
     const inflectionNo = ref(false);
     const inflectionTableContext = ref(false);
 
-        onMounted(() => {
-            watch(inflectionExpanded, (state) => {
-                console.log('hei')
-                localStorage.setItem('settings-state', JSON.stringify(state))
-              }) 
-        })
+    onMounted(() => {
+        if(localStorage.getItem('expanded') || localStorage.getItem('no') || localStorage.getItem('table-context')){
+            inflectionExpanded.value = JSON.parse(localStorage.getItem('expanded'))
+            inflectionNo.value = JSON.parse(localStorage.getItem('no'))
+            inflectionTableContext.value = JSON.parse(localStorage.getItem('table-context'))
+        }
+        watch(inflectionExpanded, () => {
+            localStorage.setItem('expanded', JSON.stringify(inflectionExpanded.value))
+        },
+        {deep: true})
+        watch(inflectionNo, () => {
+            localStorage.setItem('no', JSON.stringify(inflectionNo.value))
+        },
+        {deep: true})
+        watch(inflectionTableContext, () => {
+            localStorage.setItem('table-context', JSON.stringify(inflectionTableContext.value))
+        },
+        {deep: true})
+    })
+
+    return{
+        inflectionExpanded,
+        inflectionNo,
+        inflectionTableContext
+    }
 })

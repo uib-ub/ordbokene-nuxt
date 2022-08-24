2174b61d646b8e0ac048977880dd271ef8ec4720
Henrik Askjer <Henrik.Askjer@uib.no>
2022-08-24 14:59:31 +0200

    cleanup

--- pages/[dict]/[word].vue
+++ pages/[dict]/[word].vue
@@ -1,10 +1,7 @@
 <template>
 <div>
   <SearchForm/>
-  <NuxtLink to="/">
-    Index page
-  </NuxtLink>
-  <br>
+
   {{$route.params}}
   {{$route.query}}
 </div>
--- pages/[dict]/search.vue
+++ pages/[dict]/search.vue
@@ -1,14 +1,6 @@
 <template>
 <div>
-  <h2>Advanced search</h2>
-  <NuxtLink to="/">
-    Index page
-  </NuxtLink>
-  <br>
-  {{$route.params}}
-  {{$route.query}}
-  her {{store.query}} hei
-  PINIA: {{store.$state}}
+
 </div>
 </template>
 

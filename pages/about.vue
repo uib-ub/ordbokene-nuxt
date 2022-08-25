77525e4a2bc0200ea05746a0a72870bb2c118453
Silje Scholl <silje.scholl@uib.no>
2022-08-25 13:34:51 +0200

    added content to about

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,13 @@
 <template>
 <main class="secondary-page">
   <h2>About</h2>
-  <p>Text</p>
+  <p>This is the new website for the two Norwegian standard dictionaries <em>Bokmålsordboka</em> and <em>Nynorskordboka</em>. The website is still under development, so some features may not yet be in place. The content of the dictionaries is undergoing revision, and the editorial staff continuously publishes new word-articles, or articles which have been revised and quality checked.</p>
+  <p>Should you wish to use the old dictionary interface, you can it here: <a href="http://www.ordbok.uib.no/">ordbok.uib.no</a></p>
+  <p>In connection with the website upgrade, we have cleaned up both the interface and our data. Therefore, there may be errors and omissions in the new solution that have not yet discovered, or have had time to get sorted. Should you discover an error or a wrong or defective article, we appreciate your feedback very much! Feel free to contact us via email:</p>
+	  <p><ul class="bullet">
+		  <li>{{$t('contact.content[2]')}}<a href="mailto:ordbok@uib.no">ordbok@uib.no</a></li>
+		  <li>{{$t('contact.content[3]')}}<a href="mailto:ordbok-teknisk@uib.no">ordbok-teknisk@uib.no</a></li>
+	  </ul></p>
 
   </main>
 </template>

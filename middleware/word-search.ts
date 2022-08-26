2b04acd7dc5556edd96c0501e9ecae3a6d33d070
Henrik Askjer <Henrik.Askjer@uib.no>
2022-08-26 19:29:04 +0200

    WIP

--- middleware/word-search.ts
+++ middleware/word-search.ts
@@ -9,30 +9,43 @@ export default defineNuxtRouteMiddleware(async (to) => {
       
       }
       else {
-        const suggest = ref('')
-        suggest.value = await $fetch(`https://oda.uib.no/opal/dev/api/suggest?&q=${store.q}&dict=${store.dict}&n=20&dform=int&meta=n&include=e`)
-        store.suggest = suggest.value
-
-        store.searchUrl=to.fullPath
-        // Simple search
+    // Simple search
         if (to.params.view[0] == 'search') {
-          store.q = to.query.q
-          store.input = store.input || store.q
-          store.view = 'search'
+            console.log("SEARCH")
+            store.q = to.query.q
+            store.input = store.input || store.q
+            store.view = 'search'
         }
         else {
+            console.log("WORD", to.params.view[0])
+            console.log("INPUT", store.input)
+            store.q = to.params.view[0]
 
-          if (!store.suggest.a.exact) {
-            return "/bm,nn/search?q="+store.q
-          }
 
-          store.q = to.params.view[0]
-          store.input = store.input || store.q
-          store.view = 'word'
+            store.input =  store.input || store.q
+            store.view = 'word'
         }
-      
-        
-      
+
+        const suggest = ref('')
+        suggest.value = await $fetch(`https://oda.uib.no/opal/dev/api/suggest?&q=${store.q}&dict=${store.dict}&n=20&dform=int&meta=n&include=eis`)
+        store.suggest = suggest.value
+        let { exact, inflect } = store.suggest.a
+
+        store.searchUrl = to.fullPath
+
+        if (exact) {
+            if (exact[0][0] != store.q && exact[0][0].length == store.q.length) {
+                // kun hvis resultatet er et uttrykk eller har litt andre tegn
+                return "/bm,nn/" + exact[0][0]
+            }
+            
+        }
+        else {
+            if (inflect) {
+                return "/bm,nn/" + inflect[0][0]
+            }
+
+        }
       }
 
     console.log("STORE", store.$state)

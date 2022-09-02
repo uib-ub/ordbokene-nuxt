fac885394d6011be06027edcde919bd1e0e8bf97
Silje Scholl <silje.scholl@uib.no>
2022-09-02 11:51:22 +0200

    fix

--- stores/settingsStore.ts
+++ stores/settingsStore.ts
@@ -44,35 +44,3 @@ export const useSettingsStore = defineStore('settings', () => {
 
 
 })
-
-//export const useSettingsStore = defineStore('settings', () => {
-//const inflectionExpanded = ref(false);
-//const inflectionNo = ref(false);
-//const inflectionTableContext = ref(false);
-
-//onMounted(() => {
-//    if(localStorage.getItem('expanded') || localStorage.getItem('no') || localStorage.getItem('table-context')){
-//        inflectionExpanded.value = JSON.parse(localStorage.getItem('expanded'))
-//        inflectionNo.value = JSON.parse(localStorage.getItem('no'))
-//        inflectionTableContext.value = JSON.parse(localStorage.getItem('table-context'))
-//    }
-//    watch(inflectionExpanded, () => {
-//        localStorage.setItem('expanded', JSON.stringify(inflectionExpanded.value))
-//    },
-//    {deep: true})
-//    watch(inflectionNo, () => {
-//        localStorage.setItem('no', JSON.stringify(inflectionNo.value))
-//    },
-//    {deep: true})
-//    watch(inflectionTableContext, () => {
-//        localStorage.setItem('table-context', JSON.stringify(inflectionTableContext.value))
-//    },
-//    {deep: true})
-//})
-
-//return{
-//    inflectionExpanded,
-//    inflectionNo,
-//    inflectionTableContext
-//}
-//})

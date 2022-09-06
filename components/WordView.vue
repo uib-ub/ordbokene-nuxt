394ecc6957c1693e8496d242d7e2059c9ebe7b55
Henrik Askjer <Henrik.Askjer@uib.no>
2022-09-06 17:08:09 +0200

    fix ai search message

--- components/WordView.vue
+++ components/WordView.vue
@@ -3,12 +3,14 @@
 <script setup>
 import { useStore } from '~/stores/searchStore'
 const store = useStore()
+
+const different = store.input!= store.q && store.input
 </script>
 
 <template>
     <div>
         <h2>Word</h2>
-    <span v-if="store.q != $route.params.slug[0]">{{store.q}} er ikke et oppslagsord i {{store.dict}}. Viser ordet {{$route.params.slug[0]}}</span>
+    <span v-if="different">{{different}} er ikke et oppslagsord i {{store.dict}}. Viser ordet {{$route.params.slug[0]}}</span>
 
     </div>
 </template>

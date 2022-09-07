3d5d916a5b923b6688674fb087ace3930bd7f995
Henrik Askjer <Henrik.Askjer@uib.no>
2022-09-07 15:08:45 +0200

    article to composition api

--- components/Article.vue
+++ components/Article.vue
@@ -2,6 +2,7 @@
     <article>
         <h2 v-if="store.view != 'article'" class="dict-label">{{{"bm":"Bokmålsordboka", "nn":"Nynorskordboka"}[dict]}}</h2>
         <div class="p-3">
+        
         <h3>{{data.lemmas[0].lemma}}    </h3><button v-if="inflected" class="inflection-button py-1 px-3" type="button" data-bs-toggle="collapse" :data-bs-target="'#inflection-'+article_id" aria-expanded="false" aria-controls="collapseExample">
             {{$t('article.show_inflection')}}
         </button>
@@ -23,36 +24,32 @@
     </article>
 </template>
 
-<script>
+<script setup>
 import { useStore } from '~/stores/searchStore'
 import { useRoute } from 'vue-router'
 
+const props = defineProps({
+    article_id: Number,
+    dict: String
+})
 
-export default {
-    props: {
-        article_id: Number,
-        dict: String
-    },
-    async setup(props) {
-        const store = useStore()
-        const { pending, data } = await useAsyncData('articlee'+props.article_id, () => $fetch(`https://oda.uib.no/opal/dev/${props.dict}/article/${props.article_id}.json`))
-        return {data, pending, store}
-  
-    },
-    computed: {
-        collapsed: function() {
-            return this.$refs
+const store = useStore()
+const { pending, data } = await useAsyncData('article_'+props.article_id, () => $fetch(`https://oda.uib.no/opal/dev/${props.dict}/article/${props.article_id}.json`))
 
-        },
-        inflected: function() {
-      return this.data.lemmas.reduce((acc, lemma) => acc += lemma.paradigm_info.reduce((acc2, digm) => digm.inflection_group.includes("uninfl") ? 0 : acc2 += digm.inflection.length, 0), 0) > this.data.lemmas.length
-        },
-        lemmas_with_word_class_and_lang: function() {
-      return this.data.lemmas.map(lemma => Object.assign({language: this.dict == 'bm' ? 'nob' : 'nno',
+
+
+const inflected = computed(() => {
+    return data.value.lemmas.reduce((acc, lemma) => acc += lemma.paradigm_info.reduce((acc2, digm) => digm.inflection_group.includes("uninfl") ? 0 : acc2 += digm.inflection.length, 0), 0) > data.value.lemmas.length
+
+})
+
+const lemmas_with_word_class_and_lang = computed(() => {
+    return data.value.lemmas.map(lemma => Object.assign({language: props.dict == 'bm' ? 'nob' : 'nno',
                                                      word_class: lemma.paradigm_info[0].inflection_group.split('_')[0]}, lemma))
-    },
-    }
-}
+
+})
+
+
 </script>
 
 <style scoped>

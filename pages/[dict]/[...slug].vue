42e960bdd2f5d6cef3e487b734891a14400510d9
Henrik Askjer <Henrik.Askjer@uib.no>
2022-09-07 12:58:58 +0200

    cleanup

--- pages/[dict]/[...slug].vue
+++ pages/[dict]/[...slug].vue
@@ -3,13 +3,7 @@
   <ArticleView v-if="store.view == 'article'"/>
   <WordView v-if="store.view == 'word'"/>
   <SearchView v-if="store.view == 'search'"/>
-  <SuggestResults v-if="store.view == 'suggest'" :key ="store.q"/>
-
-  {{$route.params}}
-  {{$route.query}}
-  <br>
-  {{$route.name}}<br>
-  {{store.suggest}}
+  <SuggestResults v-if="store.view == 'suggest'"/>
 </div>
 </template>
 

5cad7503ac48f309324ce246b8f986ad59c4eb85
Silje Scholl <silje.scholl@uib.no>
2022-09-07 11:15:07 +0200

    added home link

--- app.vue
+++ app.vue
@@ -63,6 +63,7 @@
     <img class="uiblogo" src="./assets/uib-logo.svg" alt="Universitetet i Bergen, logo" />
   </div>
   <div><em>Bokmålsordboka</em>{{$t('and')}}<em>Nynorskordboka</em>{{$t('footer_description')}}
+    <NuxtLink class="btn btn-outline-tertiary" aria-current="page" to="/"><i class="bi bi-house-door-fill"></i> {{$t('home')}}</NuxtLink>
     <NuxtLink class="btn btn-outline-tertiary" aria-current="page" to="/about"><i class="bi bi-info-circle-fill"></i> {{$t('about')}}</NuxtLink>
     <NuxtLink class="btn btn-outline-tertiary" aria-current="page" to="/help"><i class="bi bi-question-circle-fill"></i> {{$t('help')}}</NuxtLink>
     <NuxtLink class="btn btn-outline-tertiary" aria-current="page" to="/settings"><i class="bi bi-gear-fill"></i> {{$t('settings.title')}}</NuxtLink>

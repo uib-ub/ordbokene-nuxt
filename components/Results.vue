5a460730532a9254d97eb06e5d78dced1a1b52ad
Henrik Askjer <Henrik.Askjer@uib.no>
2022-09-12 11:42:07 +0200

    WIP live region

--- components/Results.vue
+++ components/Results.vue
@@ -1,21 +1,33 @@
 <template>
     <div>
-    <div v-if="pending" class="d-flex align-items-center justify-content-center">
+    <div v-if="pending" class="d-flex align-items-center justify-content-center py-5 my-5">
         <div class="spinner-border text-primary" role="status">
   <span class="visually-hidden">Loading...</span>
         </div>
-
     </div>
-    <div class="row" v-if="articles">
+    <div v-if="articles && articles.meta.bm.total" aria-live="assertive" class="visually-hidden">{{articles.meta.bm.total}} treff i Bokmålsordboka</div>
+    <div v-if="articles && articles.meta.nn.total" aria-live="assertive" class="visually-hidden">{{articles.meta.nn.total}} treff i Nynorskordboka</div>
+
+    <div class="row" v-if="articles && $route.params.dict == 'bm,nn'">
       <div class="col-lg-6">
-        <div class="d-none d-lg-inline-block p-2"><h2 class="d-lg-inline-block">Bokmålsordboka</h2><span class="result-count">  | {{articles.meta.bm.total}} {{$t('results')}}</span></div>
+        <div class="d-none d-lg-inline-block p-2"><h2 class="d-lg-inline-block">Bokmålsordboka</h2><span aria-hidden="true" class="result-count">  | {{articles.meta.bm.total}} {{$t('notifications.results')}}</span></div>
       <Article v-for="(article_id, idx) in articles.articles.bm" :key="idx" :article_id="article_id" dict="bm"/>
       </div>
       <div class="col-lg-6">
+        <div class="d-none d-lg-inline-block p-2"><h2 class="d-lg-inline-block">Nynorskordboka</h2><span aria-hidden="true" class="result-count"> | {{articles.meta.nn.total}} treff</span></div>
+      <Article v-for="(article_id, idx) in articles.articles.nn" :key="idx" :article_id="article_id" dict="nn"/>
+      </div>
+    </div>
+
+    <div class="row" v-if="articles && $route.params.dict != 'bm,nn'">
+      <div v-if="$route.params.dict == 'bm'">
+        <div class="d-none d-lg-inline-block p-2"><h2 class="d-lg-inline-block">Bokmålsordboka</h2><span class="result-count">  | {{articles.meta.bm.total}} {{$t('notifications.results')}}</span></div>
+      <Article v-for="(article_id, idx) in articles.articles.bm" :key="idx" :article_id="article_id" dict="bm"/>
+      </div>
+      <div v-if="$route.params.dict == 'nn'">
         <div class="d-none d-lg-inline-block p-2"><h2 class="d-lg-inline-block">Nynorskordboka</h2><span class="result-count"> | {{articles.meta.nn.total}} treff</span></div>
       <Article v-for="(article_id, idx) in articles.articles.nn" :key="idx" :article_id="article_id" dict="nn"/>
       </div>
-
     </div>
 
 </div>

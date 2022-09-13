07ade67bc90378d64ac85e74b06d8a92026a29c3
Silje.Scholl <silje.scholl@uib.no>
2022-09-13 14:36:26 +0200

    styled content

--- components/ArticleFooter.vue
+++ components/ArticleFooter.vue
@@ -7,12 +7,26 @@ const props = defineProps({
     content_locale: String
 })
 
+onMounted(() => {
+    const showLinkCopy = navigator.clipboard;
+})
 </script>
 
 <template>
-<div>
-    <button>{{$t("article.copy_link", content_locale)}}</button>
-    <button>{{$t("article.share", content_locale)}}</button>
-    <button>{{$t("article.cite", content_locale)}}</button>
+<client-only>
+<div class="d-flex justify-content-around">
+    <button class="btn rounded-pill"><i class="bi bi-link pe-2"></i> {{$t("article.copy_link", content_locale)}}</button>
+    <button class="btn rounded-pill"><i class="bi bi-share-fill pe-2"></i> {{$t("article.share", content_locale)}}</button>
+    <button class="btn rounded-pill"><i class="bi bi-quote pe-2"></i> {{$t("article.cite", content_locale)}}</button>
 </div>
+</client-only>
 </template>
+
+<style scoped>
+.btn{
+    color: var(--bs-primary);
+    text-transform: uppercase;
+    font-size: .8rem;
+    font-weight: 600;
+}
+</style>

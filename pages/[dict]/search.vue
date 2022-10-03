5a694c34f25cbda4f57a24287d5d5bbc5fd9cfe8
Henrik Askjer <Henrik.Askjer@uib.no>
2022-10-03 14:53:34 +0200

    search component

---
+++ pages/[dict]/search.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <div class="pb-1">
+        <NuxtLink v-if="store.view == 'article' && store.searchUrl" class="back-link" :to="store.searchUrl"> <strong><BootstrapIcon icon="bi-arrow-left" color="primary"/></strong> {{$t('notifications.back')}}</NuxtLink></div>
+        <Welcome v-if="!store.q"/>
+        <main v-else>
+    
+    <Results/>
+  
+  </main>
+    </div>
+  </template>
+  
+  <script setup>
+  // pick main component: word, article or search
+  import { useStore } from '~/stores/searchStore'
+  const store = useStore()
+  
+  definePageMeta({
+    middleware: 'search-middleware'
+  })
+  </script>

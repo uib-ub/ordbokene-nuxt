9916d7a7a799aace1724226930f283ae07f3dcea
Henrik Askjer <Henrik.Askjer@uib.no>
2022-10-06 09:41:40 +0200

    adjust gradient

--- app.vue
+++ app.vue
@@ -149,7 +149,7 @@ body {
 
 #__nuxt>.welcome {
   background-image: 
-  linear-gradient(to bottom, rgba(0,0,0,.37), rgba(0,0,0,0)),
+  linear-gradient(to bottom, rgba(0,0,0,.5), rgba(21,0,9,.25), rgba(253,244,245,0), rgba(253,244,245,0)),
   url('https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80');
 }
 

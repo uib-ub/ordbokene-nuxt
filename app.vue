d2bd6564dd75d723214daac1612c23ed4ce16b7e
Henrik Askjer <Henrik.Askjer@uib.no>
2022-10-18 13:59:53 +0200

    cleanup

--- app.vue
+++ app.vue
@@ -6,7 +6,6 @@
   <div class="container-fluid mx-lg-2">
     <NuxtLink class="navbar-brand text-white" to="/">
       <div class="small mx-1 my-1 my-lg-3">
-      <div v-show="false"><h1>ordbøkene.no</h1></div>
       <div><h1 class="brand-title">Ordbøkene</h1>
       <p class="d-none d-xl-block brand-subtitle">{{$t("sub_title")}}</p>
       </div>

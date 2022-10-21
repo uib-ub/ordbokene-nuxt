88174cc73c0092cf04ec660f272987f8ac7a2e54
Silje.Scholl <silje.scholl@uib.no>
2022-10-21 09:03:05 +0200

    added content_locale to article footer

--- components/ArticleFooter.vue
+++ components/ArticleFooter.vue
@@ -100,18 +100,18 @@ const download_ris = () => {
   <div class="position-absolute top-50 start-50 translate-middle"><span>{{$t('article.link_copied', 1, { locale: content_locale})}}</span></div>
   </div>
 <div class="d-flex justify-content-around mt-3">
-    <button class="btn rounded-pill" v-if="showLinkCopy" @click="copy_link"><BootstrapIcon icon="bi-link" class="pe-2" /> {{$t("article.copy_link", 1)}}</button>
-    <button class="btn rounded-pill" v-if="webShareApiSupported" @click="shareViaWebShare"><BootstrapIcon icon="bi-share-fill" class="pe-2" /> {{$t("article.share", 1)}}</button>
-    <button class="btn rounded-pill" type="button" data-bs-toggle="collapse" :data-bs-target="'#cite-'+article_id" aria-expanded="false" aria-controls="collapseExample"><BootstrapIcon icon="bi-quote" class="pe-2" /> {{$t("article.cite", 1)}}</button>
+    <button class="btn rounded-pill" v-if="showLinkCopy" @click="copy_link" :lang="{nob: 'nb', nno: 'nn', eng: 'en'}[content_locale]"><BootstrapIcon icon="bi-link" class="pe-2" /> {{$t("article.copy_link", 1, { locale: content_locale})}}</button>
+    <button class="btn rounded-pill" v-if="webShareApiSupported" @click="shareViaWebShare" :lang="{nob: 'nb', nno: 'nn', eng: 'en'}[content_locale]"><BootstrapIcon icon="bi-share-fill" class="pe-2" /> {{$t("article.share", 1, { locale: content_locale})}}</button>
+    <button class="btn rounded-pill" type="button" data-bs-toggle="collapse" :data-bs-target="'#cite-'+article_id" aria-expanded="false" aria-controls="collapseExample" :lang="{nob: 'nb', nno: 'nn', eng: 'en'}[content_locale]"><BootstrapIcon icon="bi-quote" class="pe-2" /> {{$t("article.cite", 1, { locale: content_locale})}}</button>
 </div>
 <div class="collapse py-2" :id="'cite-'+article_id">
     <div class="cite-container card card-body">
       <h4>{{$t('article.cite_title')}}</h4>
-      <p>{{$t("article.cite_description[0]", 1)}}<em>{{$t('dicts.'+$props.dict)}}</em>{{$t("article.cite_description[1]", 1)}}</p>
+      <p :lang="{nob: 'nb', nno: 'nn', eng: 'en'}[content_locale]">{{$t("article.cite_description[0]", 1, { locale: content_locale})}}<em>{{$t('dicts.'+$props.dict)}}</em>{{$t("article.cite_description[1]", 1, { locale: content_locale})}}</p>
       <div id="citation" v-html="$t('article.citation', create_citation())" />
       <div class="pt-3">
-        <button class="btn rounded-pill" @click="copy_citation"><BootstrapIcon icon="bi-clipboard" class="pe-1" /> {{$t("article.copy", 1)}}</button>
-        <button class="btn rounded-pill" @click="download_ris"><BootstrapIcon icon="bi-download" class="pe-1" /> {{$t("article.download")}}</button>
+        <button class="btn rounded-pill" @click="copy_citation" :lang="{nob: 'nb', nno: 'nn', eng: 'en'}[content_locale]"><BootstrapIcon icon="bi-clipboard" class="pe-1" /> {{$t("article.copy", 1, { locale: content_locale})}}</button>
+        <button class="btn rounded-pill" @click="download_ris" :lang="{nob: 'nb', nno: 'nn', eng: 'en'}[content_locale]"><BootstrapIcon icon="bi-download" class="pe-1" /> {{$t("article.download", 1, { locale: content_locale})}}</button>
       </div>
     </div>
 </div>

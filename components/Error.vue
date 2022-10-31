1a404c626eb1d55ace57d4e6183d41ac4e33888a
Henrik Askjer <Henrik.Askjer@uib.no>
2022-10-31 13:51:12 +0100

    error component

---
+++ components/Error.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="callout error p-md-4">
+    <h2><BootstrapIcon class="bi-exclamation-triangle-fill" left/>{{title}}</h2>
+    <p>Feil feil feil!</p>{{error}}
+  </div>
+
+</template>
+
+<script setup>
+
+const props = defineProps({
+    error: Object
+})
+
+console.log("type", typeof props.error)
+
+const title = "Feil"
+
+</script>

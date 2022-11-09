80a7c682c360cb601ff84320f00283e71c7f2a74
Henrik Askjer <Henrik.Askjer@uib.no>
2022-11-09 14:34:16 +0100

    less padding on desktop devices

--- components/SuggestResults.vue
+++ components/SuggestResults.vue
@@ -3,7 +3,7 @@
     <h2>{{$t('notifications.similar')}}</h2>
     <ul class="nav nav-pills flex-column flex-md-row gap-md-3 pt-2">
         <li class="nav-item" v-for="(item, idx) in suggestions" :key="idx">
-            <NuxtLink class="nav-link p-3" :to="suggest_link(item[0])"><BootstrapIcon icon="bi-search" left/><span class="link-content">{{item[0]}}</span></NuxtLink>
+            <NuxtLink class="nav-link p-3 p-md-2" :to="suggest_link(item[0])"><BootstrapIcon icon="bi-search" left/><span class="link-content">{{item[0]}}</span></NuxtLink>
         </li>
     </ul>
 </div>
@@ -40,7 +40,6 @@ a {
     font-size: 1.17rem;
     letter-spacing: .1rem;
 }
-
 
 .nav-link:hover .link-content {
     border-bottom: solid 2px var(--link-decoration);

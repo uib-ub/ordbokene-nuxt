20b20fbb8027a5e29ad20ffcc0f09ae4d950e320
Henrik Askjer <Henrik.Askjer@uib.no>
2022-11-21 14:30:58 +0100

    style settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -20,29 +20,8 @@ title: t('settings.title')
 </script>
 
 <template>
-<main id="main" class="secondary-page">
+<main id="main" class="secondary-page flex flex-col gap-2">
   <h2>{{$t('settings.title')}}</h2>
-<div v-if="false" class="mb-3">
-  <div class="form-check">
-    <input class="form-check-input" type="checkbox" value="" id="checkOne" v-model="settings.$state.inflectionExpanded">
-    <label class="form-check-label" for="checkOne">
-      {{$t('settings.inflection_expanded')}}
-    </label>
-  </div>
-  <div class="form-check">
-    <input class="form-check-input" type="checkbox" value="" id="checkTwo" v-model="settings.$state.inflectionNo">
-    <label class="form-check-label" for="checkTwo">
-      {{$t('settings.inflection_no')}}
-    </label>
-  </div>
-    <div class="form-check">
-    <input class="form-check-input" type="checkbox" value="" id="checkThree" v-model="settings.$state.inflectionTableContext">
-    <label class="form-check-label" for="checkThree">
-      {{$t('settings.inflection_table_context')}}
-    </label>
-  </div>
-</div>
-
 <FormCheckbox labelId="toggle-inflection-expanded" v-model="settings.$state.inflectionExpanded" :checked="settings.$state.inflectionExpanded">
       {{$t('settings.inflection_expanded')}}
     </FormCheckbox>
@@ -54,10 +33,11 @@ title: t('settings.title')
     </FormCheckbox>
 
 
-
+<div class="mt-4">
   <button class="btn-primary" @click="resetSettings(settings)">
-    <BootstrapIcon icon="bi-trash-fill" left/><span>{{$t('settings.reset')}}</span>
+    <BootstrapIcon icon="bi-trash-fill" left primary/><span>{{$t('settings.reset')}}</span>
   </button>
+</div>
 </main>
 </template>
 

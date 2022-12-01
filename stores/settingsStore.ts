bc1843abbe93f4a96752b28b2c61fe1ec5544de8
Henrik Askjer <Henrik.Askjer@uib.no>
2022-12-01 09:33:11 +0100

    simplify settings store

--- stores/settingsStore.ts
+++ stores/settingsStore.ts
@@ -1,90 +1,16 @@
 import { defineStore } from "pinia";
 
-export const useSettingsStore = defineStore('settings', () => {
-
-    const checkLocalstorage = (key: string, defaultValue: any) => {
-        if (process.client) {
-            try {
-                let stored = localStorage.getItem(key)
-                if (stored) {
-                    return ref(typeof defaultValue === 'boolean' ?  stored == 'true' : stored)
-                }
-                else {
-                    localStorage.setItem(key, defaultValue)
-                    return ref(defaultValue)
-                }
-            }
-            catch {
-                return ref(defaultValue)
-            }
-        }
-        else {
-            return ref(defaultValue)
-        }
-    }
-
-
-    const submitSelect = useCookie<boolean>('submitSelect') || checkLocalstorage('submitSelect', false)
-    const inflectionExpanded = useCookie<boolean>('inflectionExpanded') || checkLocalstorage('inflectionExpanded', false)
-    const inflectionNo = useCookie<boolean>('inflectionNo') || checkLocalstorage('inflectionNo', false)
-    const inflectionTableContext = useCookie<boolean>('inflectionTableContext') || checkLocalstorage('inflectionNo', false)
-    const listView = useCookie<boolean>('listView')  || checkLocalstorage('inflectionNo', false)
-    
-
-    watch(submitSelect, () => {
-        try {
-            localStorage.setItem('searchSubmit', submitSelect.value ? 'true' : 'false')
-        }
-        catch {
-            console.log("No local storage")
+export const useSettingsStore = defineStore('settings', {
+    state: () => {
+        return {
+            submitSelect: false,
+            inflectionExpanded: false,
+            inflectionNo: false,
+            inflectionTableContext: false,
+            listView: false
         }
     },
-    {deep: true})
-    watch(inflectionExpanded, () => {
-        try {
-            localStorage.setItem('inflectionExpanded', inflectionExpanded.value ? 'true' : 'false')
-        }
-        catch {
-            console.log("No local storage")
-        }
-    },
-    {deep: true})
-    watch(inflectionNo, () => {
-        try {
-            localStorage.setItem('inflectionNo', inflectionNo.value ? 'true' : 'false')
-        }
-        catch {
-            console.log("No local storage")
-        }
-    },
-    {deep: true})
-    watch(inflectionTableContext, () => {
-        try {
-            localStorage.setItem('inflectionTableContext', inflectionTableContext.value ? 'true' : 'false')
-        }
-        catch {
-            console.log("No local storage")
-        }
-    },
-    {deep: true})
-
-    watch(listView, () => {
-        try {
-            localStorage.setItem('listView', listView.value ? 'true' : 'false')
-        }
-        catch {
-            console.log("No local storage")
-        }
-    },
-    {deep: true})
-
-    return  {
-        submitSelect,
-        inflectionExpanded,
-        inflectionNo,
-        inflectionTableContext,
-        listView
-    }
+    persist: true,
+  })
 
 
-})

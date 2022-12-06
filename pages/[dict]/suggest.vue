5d9b44672a9da236767ca93ca19231d74ba9d8a8
Henrik Askjer <Henrik.Askjer@uib.no>
2022-12-06 10:48:31 +0100

    translate callout

--- pages/[dict]/suggest.vue
+++ pages/[dict]/suggest.vue
@@ -2,7 +2,7 @@
       <div class="pb-1 md:mx-10">
         
   
-    <div class="callout" aria-live="polite" role="status"><CustomIcon icon="bi-info-circle-fill" left/> <strong>{{store.q}}</strong> er ikke et oppslagsord i {{store.dict}}.<div v-if="suggestions && suggestions.length" class="sr-only">Se lenkene under med forslag til andre søkeord.</div></div>
+    <div class="callout" aria-live="polite" role="status"><CustomIcon icon="bi-info-circle-fill" left/> <strong>{{store.q}}</strong>{{($t('notifications.not_a_word') + $t('dicts_inline.'+store.dict))}}.<div v-if="suggestions && suggestions.length" class="sr-only">Se lenkene under med forslag til andre søkeord.</div></div>
     <SuggestResults v-if="suggestions" :suggestions="suggestions" :key="store.q"/>
   
 

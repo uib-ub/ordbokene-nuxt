c5d4c61f99c2b5541d87f675578db75923c3e7ce
Henrik Askjer <Henrik.Askjer@uib.no>
2023-01-27 10:51:24 +0100

    WIP revamp advanced search

--- components/AdvancedSearchForm.vue
+++ components/AdvancedSearchForm.vue
@@ -1,23 +1,23 @@
 <template>
 <div class="my-2 md:mt-0">
-<form  @submit.prevent="submitForm" ref="form" class="flex flex-row flex-wrap my-2 justify-between">
-    <div class="flex flex-row gap-2">
-  <fieldset class="">
+<form  @submit.prevent="submitForm" ref="form" class="flex flex-row flex-wrap my-3 gap-6 justify-between">
+  <div class="flex flex-col sm:flex-row gap-3">
+  <fieldset class="flex flex-row lg:flex-col gap-3">
   <legend class="sr-only">Ordbok</legend>
     <FormRadio v-for="(item, idx) in dicts" :key="store.dict + idx" :model="store.dict || 'bm,nn'" @change="dict_radio" :value="item" name="dict" :labelId="'dict-radio-'+idx">
       {{$t(`dicts.${item}`)}}
     </FormRadio>
   </fieldset>
 
-<div class="">
-<div class="">
+<div class="flex flex-row lg:flex-col gap-3">
+
 <FormCheckbox labelId="inflectedCheckbox" :checked="inflection_enabled" v-model="inflection_enabled">
     {{$t('options.inflected')}}
 </FormCheckbox>
 <FormCheckbox labelId="fulltextCheckbox" :checked="fulltext_enabled" v-model="fulltext_enabled">
     {{$t('options.fulltext')}}
 </FormCheckbox>
-</div>
+
 <div class="flex flex-row gap-2">
   <label for="pos-select">{{$t('pos')}}:</label>
   <div class="select-wrapper" v-bind:class="{not_null: store.pos}">
@@ -36,12 +36,12 @@
 <div class="flex-auto" :class="{activeAutocomplete: store.autocomplete && store.autocomplete.length}">
   <Autocomplete  v-on:dropdown-submit="submitForm"/>
 </div>
-  <div class="flex flex-auto gap-2 justify-end mt-2 flex-wrap">
-  <button v-if="!(store.pos == null &&  store.scope == 'ei' && fulltext_enabled == false && inflection_enabled == true && store.dict == 'bm,nn')" class="btn btn-secondary bg-gray-500 border-gray-600 text-white" type="reset" @click="reset"> <Icon name="bi:trash" size="1.25rem" class="mr-3" />{{$t('reset')}}</button>
-  <button type="button" class="btn btn-light me-auto" v-if="store.q" @click="mini_help = !mini_help"><Icon name="bi:question-circle" class="mb-1 mr-2"/>{{$t(mini_help ? 'advanced_help_hide' : 'advanced_help')}}</button>
-  <button type="button" @click="(settings.listView = !settings.listView)" class="btn btn-light"><Icon :name='settings.listView ? "bi:list" : "bi:file-text"' class="mb-1 mr-2"/>{{settings.listView ? $t('show_articles') : $t('show_list',store.dict==='bm,nn'? 0 : 1)}}</button>
+  <div class="flex gap-2 mt-3 flex-wrap flex-col sm:justify-start sm:flex-row-reverse">
+  <button class="btn btn-primary bg-primary text-white border-primary-lighten" type="submit"> <Icon name="bi:search" size="1.25rem" class="mr-3"/>{{$t('search')}}</button>
+  <button class="btn btn-secondary bg-gray-500 border-gray-600 text-white" v-if="!(store.pos == null &&  store.scope == 'ei' && fulltext_enabled == false && inflection_enabled == true && store.dict == 'bm,nn')" type="reset" @click="reset"> <Icon name="bi:trash" size="1.25rem" class="mr-3" />{{$t('reset')}}</button>
+  <button class="btn btn-light" type="button" @click="(settings.listView = !settings.listView)" ><Icon :name='settings.listView ? "bi:list" : "bi:file-text"' class="mb-1 mr-2"/>{{settings.listView ? $t('show_articles') : $t('show_list',store.dict==='bm,nn'? 0 : 1)}}</button>
+  <button class="btn btn-light" type="button"  v-if="store.q" @click="mini_help = !mini_help"><Icon name="bi:question-circle" class="mb-1 mr-2"/>{{$t(mini_help ? 'advanced_help_hide' : 'advanced_help')}}</button>
   
-  <button class="btn btn-primary bg-primary text-white border-primary-lighten px-3" type="submit"> <Icon name="bi:search" size="1.25rem" class="mr-3"/>{{$t('search')}}</button>
   </div>
 </div>
 </form>
@@ -156,14 +156,6 @@ const submitForm = async (item) => {
 
 
 
-
-  .btn-light {
-    border-radius: 2rem;
-    padding: .25rem;
-    padding-right: 1rem;
-    padding-left: 1rem;
-
-  }
 
 
 .btn-primary i, button.btn-secondary i {

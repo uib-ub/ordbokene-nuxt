11e478e262955ba0e5082d08914db1004eb46941
Henrik Askjer <Henrik.Askjer@uib.no>
2023-01-27 10:28:59 +0100

    WIP revamp advanced search

--- components/AdvancedSearchForm.vue
+++ components/AdvancedSearchForm.vue
@@ -1,18 +1,16 @@
 <template>
 <div class="my-2 md:mt-0">
-<form  @submit.prevent="submitForm" ref="form" class="flex flex-col md:flex-row gap-10 flex-wrap">
-
-  <div class="mb-4 md:mb-3 md:pb- px-0 gap-3 md:gap-0 advanced-search w-full flex flex-wrap" >
-    <div class="mb-2 md:mb-1 p-3 md:p-0 flex flex-row gap-4 md:gap-3 lg:gap-2 flex-wrap">
-  <fieldset class="flex flex-col lg:flex-row gap-3 flex-wrap">
+<form  @submit.prevent="submitForm" ref="form" class="flex flex-row flex-wrap my-2 justify-between">
+    <div class="flex flex-row gap-2">
+  <fieldset class="">
   <legend class="sr-only">Ordbok</legend>
     <FormRadio v-for="(item, idx) in dicts" :key="store.dict + idx" :model="store.dict || 'bm,nn'" @change="dict_radio" :value="item" name="dict" :labelId="'dict-radio-'+idx">
       {{$t(`dicts.${item}`)}}
     </FormRadio>
   </fieldset>
 
-<div class="flex flex-col xl:flex-row gap-3 flex-wrap">
-<div class="flex flex-col xl:flex-row gap-3 flex-wrap">
+<div class="">
+<div class="">
 <FormCheckbox labelId="inflectedCheckbox" :checked="inflection_enabled" v-model="inflection_enabled">
     {{$t('options.inflected')}}
 </FormCheckbox>
@@ -20,7 +18,7 @@
     {{$t('options.fulltext')}}
 </FormCheckbox>
 </div>
-<div class="flex flex-row gap-3">
+<div class="flex flex-row gap-2">
   <label for="pos-select">{{$t('pos')}}:</label>
   <div class="select-wrapper" v-bind:class="{not_null: store.pos}">
   <select id="pos-select" name="pos" class="bg-tertiary" @change="store.pos = $event.target.value">
@@ -28,34 +26,25 @@
   </select>
   </div>
 </div>
-<div class="justify-end">
-<button v-if="!(store.pos == null &&  store.scope == 'ei' && fulltext_enabled == false && inflection_enabled == true && store.dict == 'bm,nn')" class="btn btn-secondary bg-gray-500 border-gray-600 text-white" type="reset" @click="reset"> <Icon name="bi:trash" size="1.25rem" class="mr-3" />{{$t('reset')}}</button>
-</div>
+
 </div>
 
 </div>
 
 
-<div class="flex justify-between flex-wrap gap-4  w-full">
+<div class="flex-1">
 <div class="flex-auto" :class="{activeAutocomplete: store.autocomplete && store.autocomplete.length}">
   <Autocomplete  v-on:dropdown-submit="submitForm"/>
 </div>
-<div class="flex">
+  <div class="flex flex-auto gap-2 justify-end mt-2 flex-wrap">
+  <button v-if="!(store.pos == null &&  store.scope == 'ei' && fulltext_enabled == false && inflection_enabled == true && store.dict == 'bm,nn')" class="btn btn-secondary bg-gray-500 border-gray-600 text-white" type="reset" @click="reset"> <Icon name="bi:trash" size="1.25rem" class="mr-3" />{{$t('reset')}}</button>
+  <button type="button" class="btn btn-light me-auto" v-if="store.q" @click="mini_help = !mini_help"><Icon name="bi:question-circle" class="mb-1 mr-2"/>{{$t(mini_help ? 'advanced_help_hide' : 'advanced_help')}}</button>
+  <button type="button" @click="(settings.listView = !settings.listView)" class="btn btn-light"><Icon :name='settings.listView ? "bi:list" : "bi:file-text"' class="mb-1 mr-2"/>{{settings.listView ? $t('show_articles') : $t('show_list',store.dict==='bm,nn'? 0 : 1)}}</button>
+  
   <button class="btn btn-primary bg-primary text-white border-primary-lighten px-3" type="submit"> <Icon name="bi:search" size="1.25rem" class="mr-3"/>{{$t('search')}}</button>
-
-</div>
-</div>
   </div>
-</form>
-<div v-if="store.q" class="flex mb-2 flex-wrap gap-2">
-  <button class="btn btn-light me-auto" v-if="store.q" @click="mini_help = !mini_help"><Icon name="bi:question-circle" class="mb-1 mr-2"/>{{$t(mini_help ? 'advanced_help_hide' : 'advanced_help')}}</button>
-
-
-  <button @click="(settings.listView = !settings.listView)" class="btn btn-light"><Icon :name='settings.listView ? "bi:list" : "bi:file-text"' class="mb-1 mr-2"/>{{settings.listView ? $t('show_articles') : $t('show_list',store.dict==='bm,nn'? 0 : 1)}}</button>
-
-
-
 </div>
+</form>
 <div v-if="!store.q || mini_help" class="secondary-page container advanced-info">
       <h2>{{$t('advanced_help')}}</h2>
       <p>Enkelt søk viser kun treff på oppslagsord i resultatlisten, og vi prøver å gi deg det beste alternativet hvis det du søker etter ikke er et oppslagsord. I avansert søk kan du derimot vise treff på bøyde former og i definisjonene (fritekstsøk). Du kan også filtrere etter ordklasse, og trunkere og kombinere søkene med spesialtegn.</p>
@@ -133,13 +122,6 @@ const submitForm = async (item) => {
 </script>
 
 <style lang="scss" scoped>
-
-.advanced-search {
-  @apply bg-tertiary md:pt-4;
-  
-  
-
-}
 
 .welcome .advanced-search {
   @apply bg-tertiary border-tertiary-darken2;

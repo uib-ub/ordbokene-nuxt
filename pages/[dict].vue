45b5a9e6140afe232233ae4e81dcd117c061caf3
Henrik Askjer <Henrik.Askjer@uib.no>
2023-02-06 10:13:20 +0100

    WIP position search nav

--- pages/[dict].vue
+++ pages/[dict].vue
@@ -1,7 +1,7 @@
 <template>
 <main id="main" tabindex="-1" class="dict-view simple-search">
-  <div class="search-nav-wrapper">    
-  <SearchNav v-if="!store.advanced"  class="md:flex md:justify-center lg:container md:mt-4"/>
+  <div class="search-nav-wrapper md:mx-auto md:px-2 lg:px-12">    
+  <SearchNav v-if="!store.advanced"  class="md:flex md:justify-center lg:justify-start lg:conainer lg:mx-auto md:mt-4"/>
 </div>
 
 

14e8d1e43b49459d69dbd35da3f9286612fa14ed
Henrik Askjer <Henrik.Askjer@uib.no>
2023-11-13 14:59:06 +0100

    generate sitemap at build time

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,12 +30,35 @@ export default defineNuxtConfig({
   },
   sitemap: {
     sitemaps: true,
-    strictNuxtContentPaths: false,
-    defaultSitemapsChunkSize: 20000,
     cacheMaxAgeSeconds: 360,
-    sources: [
-      '/api/__sitemap__/urls'
-    ],
+    defaultSitemapsChunkSize: 20000,
+    urls: async () => {
+      return await Promise.all([
+        fetch('https://ord.uib.no/bm/fil/lemma.json').then(response => response.json()),
+        fetch('https://ord.uib.no/nn/fil/lemma.json').then(response => response.json())
+      ]).then(responses => {
+
+        const [bm, nn] = responses
+        const pages = ['', 'bm', 'nn', 'search', 'help', 'about', 'contact' ]
+        const static_pages = locales.map(locale => locale + "/").concat([""]).reduce((acc, current) => acc.concat(pages.map(page => "/" + current + page)), [])
+
+        // bm,nn searches
+        const bm_searches = bm.map(item => item[0])
+        const nn_searches = new Set(nn.map(item => item[0]))
+        const searches = bm_searches.filter(item => nn_searches.has(item)).map(word => "/bm,nn/" + word)
+
+        // bm articles
+        const bm_ids = bm.map(item => item[1])
+        const bm_articles = bm_ids.map(item => "/bm/" + item)
+
+        // nn articles
+        const nn_ids = nn.map(item => item[1])
+        const nn_articles = nn_ids.map(item => "/nn/" + item)
+        
+        return [...static_pages, ...searches, ...new Set(bm_articles), ...new Set(nn_articles)]
+
+      })
+    }
   },
 
   build: {

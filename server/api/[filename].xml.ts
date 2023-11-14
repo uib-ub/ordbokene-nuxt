b5ec5d22938340662f188c7dbe9cbcb8ceb2fba8
Henrik Askjer <Henrik.Askjer@uib.no>
2023-11-14 15:22:51 +0100

    cleanup

--- server/api/[filename].xml.ts
+++ server/api/[filename].xml.ts
@@ -1,11 +1,8 @@
 export default defineEventHandler(async (event) => {
     // Remporary redirect to build time sitemap
     const filename = event.context.params["filename.xml"]
-    return "DEBUG"
     const baseurl = (process.env.VERCEL_ENV ? 'https://' : 'http://') + event.headers.get('host').toString() + (filename == "sitemap_index.xml" ? "/" : "/sitemaps/")
-    console.log(baseurl + filename)
-    return baseurl + filename
-    //return await fetch(baseurl + filename)
+    return await fetch(baseurl + filename)
 
 })
 

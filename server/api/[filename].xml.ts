7fbb1e4d7e7f4775ece159c85208df1a67b38dc7
Henrik Askjer <Henrik.Askjer@uib.no>
2023-11-14 15:33:31 +0100

    try fixing encoding error

--- server/api/[filename].xml.ts
+++ server/api/[filename].xml.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
     // Remporary redirect to build time sitemap
     const filename = event.context.params["filename.xml"]
+    event.node.res.setHeader("Content-Type", 'text/xml')
     const baseurl = (process.env.VERCEL_ENV ? 'https://' : 'http://') + event.headers.get('host').toString() + (filename == "sitemap_index.xml" ? "/" : "/sitemaps/")
     return await fetch(baseurl + filename)
 

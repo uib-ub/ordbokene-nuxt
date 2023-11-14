9ab80ab355093b767c16f234f684df421edd7016
Henrik Askjer <Henrik.Askjer@uib.no>
2023-11-14 15:20:22 +0100

    debug vercel issue

--- server/api/[filename].xml.ts
+++ server/api/[filename].xml.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
     // Remporary redirect to build time sitemap
     const filename = event.context.params["filename.xml"]
+    return "DEBUG"
     const baseurl = (process.env.VERCEL_ENV ? 'https://' : 'http://') + event.headers.get('host').toString() + (filename == "sitemap_index.xml" ? "/" : "/sitemaps/")
     console.log(baseurl + filename)
     return baseurl + filename

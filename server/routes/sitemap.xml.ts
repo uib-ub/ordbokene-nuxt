56a4e06430a5f67e2b94a378b4d018e6e9f436e8
Henrik Askjer <Henrik.Askjer@uib.no>
2023-11-15 12:27:03 +0100

    debug

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,6 +1,6 @@
 
 export default defineEventHandler(async (event) => {
-        event.node.res.setHeader("Content-Type", 'text/xml')
+        //event.node.res.setHeader("Content-Type", 'text/xml')
         const baseurl = (process.env.VERCEL_ENV ? 'https://' : 'http://') + event.headers.get('host').toString()
         return baseurl + "/api/sitemap_index.xml"
 })

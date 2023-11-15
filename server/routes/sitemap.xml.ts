10c3a43b5dfd605c68aebb35c9309bddcaa60e86
Henrik Askjer <Henrik.Askjer@uib.no>
2023-11-15 11:30:57 +0100

    return xml instead of redirect

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,5 +1,7 @@
 
 export default defineEventHandler(async (event) => {
-        await sendRedirect(event, "/api/sitemap_index.xml", 301)
+        event.node.res.setHeader("Content-Type", 'text/xml')
+        const baseurl = (process.env.VERCEL_ENV ? 'https://' : 'http://') + event.headers.get('host').toString()
+        return await fetch(baseurl + "/api/sitemap_index.xml")
 })
 

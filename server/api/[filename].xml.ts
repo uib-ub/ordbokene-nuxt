9dace1623e12bbf0276199cdd1296e510fb57521
Henrik Askjer <Henrik.Askjer@uib.no>
2023-11-15 10:15:13 +0100

    fetch sitemap from gitlab

--- server/api/[filename].xml.ts
+++ server/api/[filename].xml.ts
@@ -1,9 +1,19 @@
 export default defineEventHandler(async (event) => {
     // Remporary redirect to build time sitemap
-    const filename = event.context.params["filename.xml"]
+    let filename = event.context.params["filename.xml"]
     event.node.res.setHeader("Content-Type", 'text/xml')
-    const baseurl = (process.env.VERCEL_ENV ? 'https://' : 'http://') + event.headers.get('host').toString() + (filename == "sitemap_index.xml" ? "/" : "/sitemaps/")
-    return await fetch(baseurl + filename)
+
+    if (filename != "sitemap_index.xml") {
+        filename = "sitemaps/" + filename
+    }
+    
+    const xml = await $fetch(`https://git.app.uib.no/api/v4/projects/23985/jobs/artifacts/scheduled-sitemap/raw/${filename}?job=build-sitemap`, {
+    headers: {
+      ContentType: 'text/xml'
+    }
+  })
+    
+    return xml
 
 })
 

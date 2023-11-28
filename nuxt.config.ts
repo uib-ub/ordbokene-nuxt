a7165cd3b3d40a06e30080cf3a288b2e1c8ae676
sawravchy <43489196+sawravchy@users.noreply.github.com>
2023-11-28 10:17:43 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url'
 import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
 
 const locales = ["nob", "nno", "eng", "ukr"]
+const pages =  ['', 'bm', 'nn', 'search', 'help', 'about', 'contact' ]
 const optionalLocale = "/:locale(" + locales.join("|") + ")?"
 
 export default defineNuxtConfig({
@@ -16,6 +17,10 @@ export default defineNuxtConfig({
   ],
   nitro: {
     preset: 'node-server',
+    compressPublicAssets: true,
+    prerender: {
+      routes: locales.reduce((acc, locale) => acc.concat(pages.map(page => "/" + locale + "/" + page)), [])
+    },
     inlineDynamicImports: true,
     awsAmplify: {
 
